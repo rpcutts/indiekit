dcf86f65c608b70d44ebd223e71d08e0d1f9a592
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-11-16 15:17:24 +0000

    Mock requests for microformat unit tests

--- packages/microformats/test/url-to-mf2.test.js
+++ packages/microformats/test/url-to-mf2.test.js
@@ -1,29 +1,69 @@
+const fs = require('fs');
+const path = require('path');
+const nock = require('nock');
 const test = require('ava');
 const {urlToMf2} = require('../.');
 
-test('Throws error if URL has no items', async t => {
-  const url = 'https://paulrobertlloyd.com';
-  const error = await t.throwsAsync(urlToMf2(url));
-  t.is(error.message, 'Page has no items');
+test('Throws error if no response from URL', async t => {
+  // Mock request
+  const scope = nock('https://website.example')
+    .get('/')
+    .replyWithError('not found');
+
+  // Setup
+  const error = await t.throwsAsync(urlToMf2('https://website.example'));
+
+  // Test assertions
+  t.is(error.message, 'not found');
+  scope.done();
 });
 
-test('Throws error if no response from URL', async t => {
-  const url = 'https://example.example';
-  const error = await t.throwsAsync(urlToMf2(url));
-  t.is(error.message, 'getaddrinfo ENOTFOUND example.example');
+test('Throws error if URL has no items', async t => {
+  // Mock request
+  const file = path.resolve(__dirname, 'fixtures/html-items-none.html');
+  const html = fs.readFileSync(file, 'utf-8');
+  const scope = nock('https://website.example')
+    .get('/')
+    .reply(200, html);
+
+  // Setup
+  const error = await t.throwsAsync(urlToMf2('https://website.example'));
+
+  // Test assertions
+  t.is(error.message, 'Page has no items');
+  scope.done();
 });
 
 test('Returns empty object if requested property not found', async t => {
-  const url = 'https://paulrobertlloyd.com/notes/1550837320';
-  const mf2 = await urlToMf2(url, 'location');
+  // Mock request
+  const file = path.resolve(__dirname, 'fixtures/html-items-one.html');
+  const html = fs.readFileSync(file, 'utf-8');
+  const scope = nock('https://website.example')
+    .get('/notes/12345')
+    .reply(200, html);
+
+  // Setup
+  const mf2 = await urlToMf2('https://website.example/notes/12345', 'location');
+
+  // Test assertions
   t.deepEqual(mf2, {});
+  scope.done();
 });
 
 test('Returns requested property', async t => {
-  const url = 'https://paulrobertlloyd.com/2018/11/warp_and_weft';
-  const name = {
-    name: ['Warp and Weft']
-  };
-  const mf2 = await urlToMf2(url, 'name');
-  t.deepEqual(mf2, name);
+  // Mock request
+  const file = path.resolve(__dirname, 'fixtures/html-items-one.html');
+  const html = fs.readFileSync(file, 'utf-8');
+  const scope = nock('https://website.example')
+    .get('/notes/12345')
+    .reply(200, html);
+
+  // Setup
+  const mf2 = await urlToMf2('https://website.example/notes/12345', 'name');
+
+  // Test assertions
+  t.deepEqual(mf2, {
+    name: ['I ate a cheese sandwich.']
+  });
+  scope.done();
 });
